ebefde65123da972b1644e3a33e848c056d763cd
Hermann Kaser <hermann.kaser@gmail.com>
2010-01-26 23:08:29 +0100

    jQuery + jQuery UI includes

--- system/application/views/skeleton.php
+++ system/application/views/skeleton.php
@@ -3,6 +3,15 @@
 		<title><?=$title?></title>
 		
 		<link rel="stylesheet" type="text/css" href="<?=base_url();?>/static/css/default.css" />
+		<link href="/static/css/base/ui.base.css" type="text/css" rel="stylesheet" />
+		<link href="/static/css/base/ui.theme.css" type="text/css" rel="stylesheet" />
+		
+		<script src="http://www.google.com/jsapi" language="javascript"></script> 
+		<script language="javascript"> 
+			google.load("jquery","1.4.0");
+			google.load("jqueryui", "1.7.2");
+		</script>
+        
 	</head>
 	<body>
 		<div id="navbar">
