6dbce27776dae2ea22578d45d8af284874e0cf16
Hermann Kaser <hermann.kaser@gmail.com>
2010-02-27 20:05:25 +0100

    Date format

--- system/application/views/tournaments/index.php
+++ system/application/views/tournaments/index.php
@@ -6,7 +6,7 @@
 	<ul>
 		<?php foreach($future_tournaments->result() as $tournament): ?>
 			<li><a href="<?=site_url('/tournament/view/'.$tournament->id)?>">
-				<?=$tournament->name?></a> on <?=mdate('%F %j%S %Y', mysql_to_unix($tournament->date))?>
+				<?=$tournament->name?></a> on <?=mdate('%F %j%S, %Y', mysql_to_unix($tournament->date))?>
 				(Players: <?=$this->tournament_model->countSignedUp($tournament->id)?>)
 			</li>
 		<?php endforeach; ?>
@@ -21,7 +21,7 @@
 	<ul>
 		<?php foreach($past_tournaments->result() as $tournament): ?>
 			<li><a href="<?=site_url('/tournament/view/'.$tournament->id)?>">
-				<?=$tournament->name?></a> on <?=mdate('%F %j%S %Y', mysql_to_unix($tournament->date))?>
+				<?=$tournament->name?></a> on <?=mdate('%F %j%S, %Y', mysql_to_unix($tournament->date))?>
 				(Players: <?=$this->tournament_model->countSignedUp($tournament->id)?>)
 			</li>
 		<?php endforeach; ?>
