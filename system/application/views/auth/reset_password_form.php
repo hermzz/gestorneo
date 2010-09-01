0b963e0e6f4080027c5e5f77a7dd5e13f1b48927
Hermann Kaser <hermann.kaser@gmail.com>
2010-09-01 21:57:11 +0200

    Translate rest of auth strings

--- system/application/views/auth/reset_password_form.php
+++ system/application/views/auth/reset_password_form.php
@@ -15,15 +15,15 @@ $confirm_new_password = array(
 <?php echo form_open($this->uri->uri_string()); ?>
 <table>
 	<tr>
-		<td><?php echo form_label('New Password', $new_password['id']); ?></td>
+		<td><?php echo form_label(_('New Password'), $new_password['id']); ?></td>
 		<td><?php echo form_password($new_password); ?></td>
 		<td style="color: red;"><?php echo form_error($new_password['name']); ?><?php echo isset($errors[$new_password['name']])?$errors[$new_password['name']]:''; ?></td>
 	</tr>
 	<tr>
-		<td><?php echo form_label('Confirm New Password', $confirm_new_password['id']); ?></td>
+		<td><?php echo form_label(_('Confirm New Password'), $confirm_new_password['id']); ?></td>
 		<td><?php echo form_password($confirm_new_password); ?></td>
 		<td style="color: red;"><?php echo form_error($confirm_new_password['name']); ?><?php echo isset($errors[$confirm_new_password['name']])?$errors[$confirm_new_password['name']]:''; ?></td>
 	</tr>
 </table>
-<?php echo form_submit('change', 'Change Password'); ?>
+<?php echo form_submit('change', _('Change Password')); ?>
 <?php echo form_close(); ?>
