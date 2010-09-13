af27b29178bc685bd4add41d478ce01153a70e18
Hermann Kaser <hermann.kaser@gmail.com>
2010-09-13 20:59:08 +0200

    Temporarlily remove captcha

--- system/application/config/tank_auth.php
+++ system/application/config/tank_auth.php
@@ -28,7 +28,7 @@ $config['webmaster_email'] = 'hermann.kaser@gmail.com';
 |--------------------------------------------------------------------------
 */
 $config['allow_registration'] = TRUE;
-$config['captcha_registration'] = TRUE;
+$config['captcha_registration'] = FALSE;
 $config['email_activation'] = FALSE;
 $config['email_activation_expire'] = 60*60*24*2;
 $config['email_account_details'] = TRUE;
