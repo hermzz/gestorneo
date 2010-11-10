6d5ae1e24b8dc6e0e5eba0a5397b93ec087df91f
hermzz <hermann.kaser@gmail.com>
2010-11-10 23:57:55 +0100

    Update DB structure

--- system/sql/gestorneo.sql
+++ system/sql/gestorneo.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.1.49, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: gestorneo
 -- ------------------------------------------------------
--- Server version	5.1.41-3ubuntu12
+-- Server version	5.1.49-1ubuntu8
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -33,16 +33,6 @@ CREATE TABLE `ci_sessions` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ci_sessions`
---
-
-LOCK TABLES `ci_sessions` WRITE;
-/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
-INSERT INTO `ci_sessions` VALUES ('056e537d60bfb255839fd9684cdff9a3','127.0.0.1','Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWeb',1273528209,'a:4:{s:7:\"user_id\";s:1:\"1\";s:8:\"username\";s:7:\"Hermann\";s:6:\"status\";s:1:\"1\";s:5:\"level\";s:5:\"admin\";}');
-/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `login_attempts`
 --
 
@@ -59,63 +49,19 @@ CREATE TABLE `login_attempts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `login_attempts`
+-- Table structure for table `teams`
 --
 
-LOCK TABLES `login_attempts` WRITE;
-/*!40000 ALTER TABLE `login_attempts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `login_attempts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `player2tournament`
---
-
-DROP TABLE IF EXISTS `player2tournament`;
+DROP TABLE IF EXISTS `teams`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player2tournament` (
-  `pid` int(11) NOT NULL DEFAULT '0',
-  `tid` int(11) NOT NULL DEFAULT '0',
-  `confirmed` tinyint(4) DEFAULT '0',
-  PRIMARY KEY (`pid`,`tid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `player2tournament`
---
-
-LOCK TABLES `player2tournament` WRITE;
-/*!40000 ALTER TABLE `player2tournament` DISABLE KEYS */;
-INSERT INTO `player2tournament` VALUES (1,1,0),(1,3,1),(1,4,1),(1,5,1);
-/*!40000 ALTER TABLE `player2tournament` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `players`
---
-
-DROP TABLE IF EXISTS `players`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `players` (
+CREATE TABLE `teams` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `joined` date DEFAULT NULL,
+  `description` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `players`
---
-
-LOCK TABLES `players` WRITE;
-/*!40000 ALTER TABLE `players` DISABLE KEYS */;
-INSERT INTO `players` VALUES (1,'Hermann','2008-02-01'),(2,'Justin','2007-06-01'),(3,'Jose Antonio','2000-10-12'),(4,'Alberto','2006-03-22');
-/*!40000 ALTER TABLE `players` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `tournament_players`
@@ -128,19 +74,24 @@ CREATE TABLE `tournament_players` (
   `pid` int(11) NOT NULL DEFAULT '0',
   `tid` int(11) NOT NULL DEFAULT '0',
   `confirmed` tinyint(4) DEFAULT '0',
+  `team_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`pid`,`tid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tournament_players`
+-- Table structure for table `tournament_teams`
 --
 
-LOCK TABLES `tournament_players` WRITE;
-/*!40000 ALTER TABLE `tournament_players` DISABLE KEYS */;
-INSERT INTO `tournament_players` VALUES (1,1,0),(1,3,0),(1,4,1),(1,5,0),(2,3,0),(2,5,1),(3,3,0),(3,5,1),(3,6,0);
-/*!40000 ALTER TABLE `tournament_players` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `tournament_teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tournament_teams` (
+  `tid` int(11) NOT NULL,
+  `teid` int(11) NOT NULL,
+  PRIMARY KEY (`tid`,`teid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `tournaments`
@@ -157,18 +108,8 @@ CREATE TABLE `tournaments` (
   `end_date` date NOT NULL,
   `signup_deadline` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tournaments`
---
-
-LOCK TABLES `tournaments` WRITE;
-/*!40000 ALTER TABLE `tournaments` DISABLE KEYS */;
-INSERT INTO `tournaments` VALUES (1,'1ª CAC','','2010-05-15','2010-05-15','2010-05-06'),(2,'Cremas Beach Challenge','Women only.','2010-03-05','2010-03-06','2010-02-26'),(3,'Costa Brava Tourney','','2010-03-27','2010-03-29','2010-03-19'),(4,'Horadada In&Out','','2010-01-30','2010-01-31','2010-01-22'),(5,'La Abuela','','2010-04-17','2010-04-18','2010-04-02'),(6,'La Liga Open','','2010-05-29','2010-05-30','2010-05-14');
-/*!40000 ALTER TABLE `tournaments` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user_autologin`
@@ -188,15 +129,6 @@ CREATE TABLE `user_autologin` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_autologin`
---
-
-LOCK TABLES `user_autologin` WRITE;
-/*!40000 ALTER TABLE `user_autologin` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_autologin` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user_profiles`
 --
 
@@ -209,18 +141,8 @@ CREATE TABLE `user_profiles` (
   `country` varchar(20) COLLATE utf8_bin DEFAULT NULL,
   `website` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_profiles`
---
-
-LOCK TABLES `user_profiles` WRITE;
-/*!40000 ALTER TABLE `user_profiles` DISABLE KEYS */;
-INSERT INTO `user_profiles` VALUES (1,2,NULL,NULL),(2,3,NULL,NULL);
-/*!40000 ALTER TABLE `user_profiles` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -245,21 +167,11 @@ CREATE TABLE `users` (
   `last_login` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `level` enum('user','t_admin','admin') COLLATE utf8_bin NOT NULL DEFAULT 'admin',
+  `level` enum('user','t_admin','admin') COLLATE utf8_bin NOT NULL DEFAULT 'user',
   `sex` enum('M','F') COLLATE utf8_bin NOT NULL DEFAULT 'M',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Hermann','$P$BvAan8tBKA4CDjtrgRQF.iqK3Zt.Ke/','hermann.kaser@gmail.com',1,0,NULL,NULL,NULL,NULL,'766ec0e8b4630f2014e4ca70e5d83e4d','127.0.0.1','2010-05-10 23:04:55','2010-02-28 11:58:00','2010-05-10 21:04:55','admin','M'),(2,'JPalm','$P$BjCSllVIK5yyHQXfAB/15n1iksHMWm1','ajpalmer@gmail.com',1,0,NULL,NULL,NULL,NULL,NULL,'88.26.196.160','2010-03-23 17:37:06','2010-03-23 17:36:34','2010-04-14 21:17:09','user','M'),(3,'Jose','$P$B5.jIAeWHDz29BxsmS5GfkjlpvZmiH1','joseantoniodelosrios@yahoo.fr',1,0,NULL,NULL,NULL,NULL,NULL,'213.97.255.249','2010-03-24 11:13:24','2010-03-24 11:12:53','2010-04-14 21:17:09','user','M');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -270,4 +182,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-05-10 23:51:51
+-- Dump completed on 2010-11-10 23:57:34
