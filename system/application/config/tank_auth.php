b37533f19ce7bc6105703f5fa09b127bc45c2afb
Hermann Kaser <hermann.kaser@gmail.com>
2011-03-05 22:45:38 +0100

    Change minimum username and password lengths

--- system/application/config/tank_auth.php
+++ system/application/config/tank_auth.php
@@ -35,9 +35,9 @@ $config['email_account_details'] = TRUE;
 $config['use_username'] = TRUE;
 
 $config['username_min_length'] = 4;
-$config['username_max_length'] = 20;
+$config['username_max_length'] = 100;
 $config['password_min_length'] = 4;
-$config['password_max_length'] = 20;
+$config['password_max_length'] = 30;
 
 /*
 |--------------------------------------------------------------------------
