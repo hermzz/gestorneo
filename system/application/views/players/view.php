909cb4718f9436f3c154ceb097db22984bf77149
Hermann Kaser <hermann.kaser@gmail.com>
2011-03-10 21:44:56 +0000

    Separate tournaments by month/year

--- system/application/views/players/view.php
+++ system/application/views/players/view.php
@@ -10,8 +10,22 @@
 	<?php endif; ?>
 	
 	<?php if($tournaments): ?>
-		<ul>
+		<?php $month_year = false; ?>
 			<?php foreach($tournaments as $tournament): ?>
+				<?php
+					if(!$month_year)
+					{
+						echo '<h3>'.strftime('%B %Y', mysql_to_unix($tournament->start_date)).'</h3><ul>';
+						$month_year = strftime('%m-%Y', mysql_to_unix($tournament->start_date));
+					} else {
+						$t_month_year = strftime('%m-%Y', mysql_to_unix($tournament->start_date));
+						if($t_month_year != $month_year)
+						{
+							echo '</ul><h3>'.strftime('%B %Y', mysql_to_unix($tournament->start_date)).'</h3><ul>';
+							$month_year = $t_month_year;
+						}
+					}
+				?>
 				<li><a href="/tournament/view/<?=$tournament->id?>"><?=$tournament->name?></a>
 					 - <?=strftime('%A %e, %B %Y', mysql_to_unix($tournament->start_date))?></li>
 			<?php endforeach; ?>
