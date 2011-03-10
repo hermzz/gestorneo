31603caf90cbaec4f6ceb15c7aa22a1bd36f128a
Hermann Kaser <hermann.kaser@gmail.com>
2011-03-10 22:10:49 +0000

    Use the date picker properly

--- system/application/views/tournaments/edit.php
+++ system/application/views/tournaments/edit.php
@@ -3,40 +3,16 @@
 <link href="/static/css/base/ui.datepicker.css" type="text/css" rel="stylesheet" />
 <script type="text/javascript">
 	$(document).ready(function(){
-		$('#start_calendar').click(function() {
-			$('#start_datepicker').toggle();
-		});
-	
-		$("#start_datepicker").datepicker({
+		$("#start_date").datepicker({
 			dateFormat: 'dd/mm/yy',
-			onSelect: function(dateText, inst) {
-				$('#start_date').attr('value', dateText);
-				$('#start_datepicker').hide();
-			}
 		});
 		
-		$('#end_calendar').click(function() {
-			$('#end_datepicker').toggle();
-		});
-		
-		$("#end_datepicker").datepicker({
+		$("#end_date").datepicker({
 			dateFormat: 'dd/mm/yy',
-			onSelect: function(dateText, inst) {
-				$('#end_date').attr('value', dateText);
-				$('#end_datepicker').hide();
-			}
 		});
 		
-		$('#deadline_calendar').click(function() {
-			$('#deadline_datepicker').toggle();
-		});
-		
-		$("#deadline_datepicker").datepicker({
+		$("#deadline_date").datepicker({
 			dateFormat: 'dd/mm/yy',
-			onSelect: function(dateText, inst) {
-				$('#deadline_date').attr('value', dateText);
-				$('#deadline_datepicker').hide();
-			}
 		});
 	});
 </script>
@@ -48,22 +24,13 @@
     <input type="text" id="name" name="name" value="<?=set_value('name', $tournament->name);?>" /><br />
 
     <label for="start_date"><?=_('Start date');?></label>
-    <input type="text" id="start_date" name="start_date" value="<?=set_value('start_date', strftime('%d/%m/%Y', mysql_to_unix($tournament->start_date)));?>" />
-    <a href="#" id="start_calendar"><img src="/static/images/calendar.png" /></a>
-    
-    <div class="datepicker" id="start_datepicker">&nbsp;</div><br />
+    <input type="text" id="start_date" name="start_date" value="<?=set_value('start_date', strftime('%d/%m/%Y', mysql_to_unix($tournament->start_date)));?>" /><br />
 
     <label for="end_date"><?=_('End date');?></label>
-    <input type="text" id="end_date" name="end_date" value="<?=set_value('end_date', strftime('%d/%m/%Y', mysql_to_unix($tournament->end_date)));?>" />
-    <a href="#" id="end_calendar"><img src="/static/images/calendar.png" /></a>
-    
-    <div class="datepicker" id="end_datepicker">&nbsp;</div><br />
+    <input type="text" id="end_date" name="end_date" value="<?=set_value('end_date', strftime('%d/%m/%Y', mysql_to_unix($tournament->end_date)));?>" /><br />
 
     <label for="deadline_date"><?=_('Signup deadline');?></label>
-    <input type="text" id="deadline_date" name="deadline_date" value="<?=set_value('deadline_date', strftime('%d/%m/%Y', mysql_to_unix($tournament->signup_deadline)));?>" />
-    <a href="#" id="deadline_calendar"><img src="/static/images/calendar.png" /></a>
-    
-    <div class="datepicker" id="deadline_datepicker">&nbsp;</div><br />
+    <input type="text" id="deadline_date" name="deadline_date" value="<?=set_value('deadline_date', strftime('%d/%m/%Y', mysql_to_unix($tournament->signup_deadline)));?>" /><br />
     
     <label for="notes"><?=_('Notes');?></label>
     <textarea id="notes" name="notes" rows="20" cols="60"><?=set_value('notes', $tournament->notes);?></textarea><br />
