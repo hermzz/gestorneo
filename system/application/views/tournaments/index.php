3bf14076749e2188d8883859f89f23f98fb92802
Hermann Kaser <hermann.kaser@gmail.com>
2011-03-10 21:43:19 +0000

    Separate tournaments by month/year

--- system/application/views/tournaments/index.php
+++ system/application/views/tournaments/index.php
@@ -5,10 +5,24 @@
 <h2><?=_('Upcoming tournaments');?></h2>
 
 <?php if($future_tournaments): ?>
-	<ul>
+	<?php $month_year = false; ?>
 		<?php foreach($future_tournaments as $tournament): ?>
+			<?php
+				if(!$month_year)
+				{
+					echo '<h3>'.strftime('%B %Y', mysql_to_unix($tournament->start_date)).'</h3><ul>';
+					$month_year = strftime('%m-%Y', mysql_to_unix($tournament->start_date));
+				} else {
+					$t_month_year = strftime('%m-%Y', mysql_to_unix($tournament->start_date));
+					if($t_month_year != $month_year)
+					{
+						echo '</ul><h3>'.strftime('%B %Y', mysql_to_unix($tournament->start_date)).'</h3><ul>';
+						$month_year = $t_month_year;
+					}
+				}
+			?>
 			<li>
-				<?=sprintf(_('<a href="%s">%s</a> on %s'), site_url('/tournament/view/'.$tournament->id), $tournament->name, strftime('%A %e, %B %Y', mysql_to_unix($tournament->start_date)));?>
+				<?=sprintf(_('<a href="%s">%s</a> on %s'), site_url('/tournament/view/'.$tournament->id), $tournament->name, strftime('%A %e', mysql_to_unix($tournament->start_date)));?>
 				(<?=_('Players');?>: <?=$this->tournament_model->countSignedUp($tournament->id)?>
 					[<?=$this->tournament_model->countSignedUp($tournament->id, 'M')?>M/
 					<?=$this->tournament_model->countSignedUp($tournament->id, 'F')?>F])
@@ -22,8 +36,22 @@
 <h2><?=_('Past tournaments');?></h2>
 
 <?php if($past_tournaments): ?>
-	<ul>
+	<?php $month_year = false; ?>
 		<?php foreach($past_tournaments as $tournament): ?>
+			<?php
+				if(!$month_year)
+				{
+					echo '<h3>'.strftime('%B %Y', mysql_to_unix($tournament->start_date)).'</h3><ul>';
+					$month_year = strftime('%m-%Y', mysql_to_unix($tournament->start_date));
+				} else {
+					$t_month_year = strftime('%m-%Y', mysql_to_unix($tournament->start_date));
+					if($t_month_year != $month_year)
+					{
+						echo '</ul><h3>'.strftime('%B %Y', mysql_to_unix($tournament->start_date)).'</h3><ul>';
+						$month_year = $t_month_year;
+					}
+				}
+			?>
 			<li>
 				<?=sprintf(_('<a href="%s">%s</a> on %s'), site_url('/tournament/view/'.$tournament->id), $tournament->name, strftime('%A %e, %B %Y', mysql_to_unix($tournament->start_date)));?>
 				(<?=_('Players');?>: <?=$this->tournament_model->countSignedUp($tournament->id)?>
