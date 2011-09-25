d03ffee53c0614e3d8a24bd75dac217383a14766
hermzz <hermann.kaser@gmail.com>
2011-09-25 21:42:11 +0200

    Ajaxify edit tournament

--- system/application/views/tournaments/edit.php
+++ system/application/views/tournaments/edit.php
@@ -14,6 +14,116 @@
 		$("#deadline_date").datepicker({
 			dateFormat: 'dd/mm/yy',
 		});
+		
+		$('input[name="teams_autocomplete"]').autocomplete({
+			source: function(request, response) {
+				$.ajax({
+					url: '/ajax/team_autocomplete',
+					dataType: "jsonp",
+					data: {
+						term: request.term
+					},
+					success: function(data) 
+					{
+						if(data.success)
+						{
+							response($.map(data.results, function(item) 
+							{
+								return {
+									label: item.name,
+									value: item.id
+								}
+							}));
+						} else {
+							console.log('fail');
+						}
+					}
+				});
+			},
+			select: function(event, ui) 
+			{
+				if($('#teams_container .teams').length == 0)
+				{
+					$('#teams_container').html('');
+				}
+				
+				$('#teams_container').append(
+					'<li class="teams r-'+ui.item.value+'">' + ui.item.label + 
+					'<input type="hidden" name="teams[]" value="'+ui.item.value+'" /'+'>' +
+					' [<a href="#">x</a>]</li>'
+				);
+				
+				$('#teams_container .r-'+ ui.item.value+' a').click(function (e) {
+					$(e.target).parent().remove();
+					
+					return false;
+				});
+			},
+			close: function() {	
+				$('input[name="teams_autocomplete"]').val('');
+			}
+		});
+		
+		$('#teams_container a').click(function (e) {
+			$(e.target).parent().remove();
+			
+			return false;
+		});
+		
+		$('input[name="players_autocomplete"]').autocomplete({
+			source: function(request, response) {
+				$.ajax({
+					url: '/ajax/player_autocomplete',
+					dataType: "jsonp",
+					data: {
+						term: request.term
+					},
+					success: function(data) 
+					{
+						if(data.success)
+						{
+							response($.map(data.results, function(item) 
+							{
+								return {
+									label: item.name,
+									value: item.id
+								}
+							}));
+						} else {
+							console.log('fail');
+						}
+					}
+				});
+			},
+			select: function(event, ui) 
+			{
+				if($('#players_container .players').length == 0)
+				{
+					$('#players_container').html('');
+				}
+				
+				$('#players_container').append(
+					'<li class="players r-'+ui.item.value+'">' + ui.item.label + 
+					'<input type="hidden" name="admins[]" value="'+ui.item.value+'" /'+'>' +
+					' [<a href="#">x</a>]</li>'
+				);
+				
+				$('#players_container .r-'+ ui.item.value+' a').click(function (e) {
+					$(e.target).parent().remove();
+					
+					return false;
+				});
+			},
+			close: function() {	
+				$('input[name="players_autocomplete"]').val('');
+			}
+		});
+		
+		$('#players_container a').click(function (e) {
+			$(e.target).parent().remove();
+			
+			return false;
+		});
 	});
 </script>
 
@@ -37,20 +147,42 @@
     
     <fieldset>
     	<legend><?=_('Teams');?></legend>
-		<?php foreach($teams as $team): ?>
-			<input type="checkbox" id="team-<?=$team->id;?>" name="teams[]" value="<?=$team->id;?>" <?=set_checkbox('teams[]', $team->id, in_array($team->id, $selected_teams));?> />
-			<label for="team-<?=$team->id;?>" class="checkbox"><?=$team->name;?></label><br />
-		<?php endforeach; ?>
+    	<input type="text" name="teams_autocomplete" />
+    	<ul id="teams_container">
+    		<?php if($teams): ?>
+    			<?php foreach($teams as $team): ?>
+    				<?php if(in_array($team->id, $selected_teams)): ?>
+    					<li class="teams r-<?=$team->id;?>">
+    						<?=$team->name;?>
+							<input type="hidden" name="teams[]" value="<?=$team->id;?>" />
+							[<a href="#">x</a>]
+						</li>
+					<?php endif; ?>
+				<?php endforeach; ?>
+    		<?php else: ?>
+	    		<li>No teams selected</li>
+	    	<?php endif; ?>
+    	</ul>
     </fieldset>
     
     <fieldset>
     	<legend><?=_('Admins');?></legend>
-		<?php foreach($users as $user): ?>
-			<?php if($user->level == 'user'): ?>
-				<input type="checkbox" id="user-<?=$user->id;?>" name="admin_users[]" value="<?=$user->id;?>" <?=set_checkbox('admin_users[]', $user->id, in_array($user->id, $tournament_admins));?> />
-				<label for="user-<?=$user->id;?>" class="checkbox"><?=$user->username;?></label><br />
-			<?php endif; ?>
-		<?php endforeach; ?>
+    	<input type="text" name="players_autocomplete" />
+    	<ul id="players_container">
+    		<?php if($users): ?>
+    			<?php foreach($users as $user): ?>
+    				<?php if(in_array($user->id, $tournament_admins)): ?>
+    					<li class="players r-<?=$user->id;?>">
+    						<?=$user->username;?>
+							<input type="hidden" name="admins[]" value="<?=$user->id;?>" />
+							[<a href="#">x</a>]
+						</li>
+					<?php endif; ?>
+				<?php endforeach; ?>
+    		<?php else: ?>
+	    		<li>No players selected</li>
+	    	<?php endif; ?>
+    	</ul>
     </fieldset>
 
     <input type="submit" name="submitNewTournament" value="<?=_('Edit');?>" />
