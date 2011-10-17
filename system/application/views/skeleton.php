f576ad84b60560e5c504de59273eb4e9146b118f
hermzz <hermann.kaser@gmail.com>
2011-10-17 21:31:26 +0200

    Fix path

--- system/application/views/skeleton.php
+++ system/application/views/skeleton.php
@@ -2,17 +2,17 @@
 	<head>
 		<title><?=$title?></title>
 		
-		<link rel="stylesheet" type="text/css" href="<?=base_url();?>/static/css/default.css" />
-		<link rel="stylesheet" type="text/css" href="<?=base_url();?>/static/css/bootstrap.min.css" />
-		<link href="<?=base_url();?>/static/css/ui-lightness/ui.base.css" type="text/css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="<?=base_url();?>static/css/default.css" />
+		<link rel="stylesheet" type="text/css" href="<?=base_url();?>static/css/bootstrap.min.css" />
+		<link href="<?=base_url();?>static/css/ui-lightness/ui.base.css" type="text/css" rel="stylesheet" />
 		
-		<script src="<?=base_url();?>/static/javascript/jquery.min.js" language="javascript"></script>
-		<script src="<?=base_url();?>/static/javascript/jquery-ui.min.js" language="javascript"></script>
-		<script src="<?=base_url();?>/static/javascript/jquery.cookie.js" language="javascript"></script>
-		<script src="<?=base_url();?>/static/javascript/jquery.editinplace.js" language="javascript"></script>
-		<script src="<?=base_url();?>/static/javascript/jquery.editinplace.js" language="javascript"></script>
+		<script src="<?=base_url();?>static/javascript/jquery.min.js" language="javascript"></script>
+		<script src="<?=base_url();?>static/javascript/jquery-ui.min.js" language="javascript"></script>
+		<script src="<?=base_url();?>static/javascript/jquery.cookie.js" language="javascript"></script>
+		<script src="<?=base_url();?>static/javascript/jquery.editinplace.js" language="javascript"></script>
+		<script src="<?=base_url();?>static/javascript/jquery.editinplace.js" language="javascript"></script>
 		
-		<script src="<?=base_url();?>/static/javascript/bootstrap-dropdown.js" language="javascript"></script>
+		<script src="<?=base_url();?>static/javascript/bootstrap-dropdown.js" language="javascript"></script>
 		
 		<script>
 			$(document).ready(function() {
