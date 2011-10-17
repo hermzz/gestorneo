ce0968a145cb76eb852aae910a9573ad91654977
hermzz <hermann.kaser@gmail.com>
2011-10-17 22:49:36 +0200

    Bootstrapify team create/edit pages

--- system/application/views/teams/create.php
+++ system/application/views/teams/create.php
@@ -3,13 +3,22 @@
 <?=validation_errors()?>
 
 <form action="#" method="post">
-    <label for="name"><?=_('Name');?></label>
-    <input type="text" id="name" name="name" value="<?=set_value('name');?>" /><br />
-    
-    <label for="description"><?=_('Description');?></label><br />
-    <textarea id="description" name="description" rows="10" cols="60"><?=set_value('description');?></textarea><br />
-    
-    <p><a href="/misc/page/markdown_help" target="_blank"><?=_('markdown help');?></a></p>
-
-    <input type="submit" name="submitNewTeam" value="<?=_('Add');?>" />
+	<fieldset>
+		<div class="clearfix">
+			<label for="name"><?=_('Name');?></label>
+			<div class="input">
+				<input type="text" id="name" name="name" value="<?=set_value('name');?>" />
+			</div>
+		</div>
+		
+		<div class="clearfix">
+			<label for="description"><?=_('Description');?></label>
+			<div class="input">
+				<textarea id="description" name="description" rows="8" cols="60" class="span6"><?=set_value('description');?></textarea>
+			    <p><a href="/misc/page/markdown_help" target="_blank"><?=_('markdown help');?></a></p>
+			</div>
+		</div>
+		
+	    <input type="submit" name="submitNewTeam" value="<?=_('Add');?>" class="btn primary offset2" />
+    </fieldset>
 </form>
--- system/application/views/teams/edit.php
+++ system/application/views/teams/edit.php
@@ -3,13 +3,22 @@
 <?=validation_errors()?>
 
 <form action="#" method="post">
-    <label for="name"><?=_('Name');?></label>
-    <input type="text" id="name" name="name" value="<?=set_value('name', $team->name);?>" /><br />
-    
-    <label for="description"><?=_('Description');?></label><br />
-    <textarea id="description" name="description" rows="10" cols="60"><?=set_value('description', $team->description);?></textarea><br />
-    
-    <p><a href="/misc/page/markdown_help" target="_blank"><?=_('markdown help');?></a></p>
-
-    <input type="submit" name="submitEditTeam" value="<?=_('Edit');?>" />
+	<fieldset>
+		<div class="clearfix">
+			<label for="name"><?=_('Name');?></label>
+			<div class="input">
+				<input type="text" id="name" name="name" value="<?=set_value('name', $team->name);?>" />
+			</div>
+		</div>
+		
+		<div class="clearfix">
+			<label for="description"><?=_('Description');?></label>
+			<div class="input">
+				<textarea id="description" name="description" rows="8" cols="60" class="span6"><?=set_value('description', $team->description);?></textarea>
+			    <p><a href="/misc/page/markdown_help" target="_blank"><?=_('markdown help');?></a></p>
+			</div>
+		</div>
+		
+	    <input type="submit" name="submitEditTeam" value="<?=_('Edit');?>" class="btn primary offset2" />
+    </fieldset>
 </form>
