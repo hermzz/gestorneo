9c1ea72ec713fcd89c01f8e5cd75309029d37b90
hermzz <hermann.kaser@gmail.com>
2012-04-07 20:41:08 +0200

    Adapt navbar to new bootstrap

--- system/application/views/navbar.php
+++ system/application/views/navbar.php
@@ -1,26 +1,29 @@
-<div class="topbar">
-	<div class="fill">
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
 		<div class="container">
-			<ul class="nav">
-				<li><a href="/"><?=_('home');?></a></li>
-    
-				<?php if($this->tank_auth->is_logged_in()): ?>
-					<li><a href="/tournament/"><?=_('tournaments');?></a></li>
-					<li><a href="/player/"><?=_('players');?></a></li>
-					<li><a href="/team/"><?=_('teams');?></a></li>
-					<li><a href="/auth/logout"><?=_('Logout');?></a></li>
-				<?php endif; ?>
-			</ul>
-			
-			<form action="#" class="pull-right">
-				<select name="language_chooser" class="input-small">
-					<?php foreach($languages as $key => $name): ?>
-						<option value="<?=$key?>" <?=$key==$selected_language?'selected="selected"':'';?>>
-							<?=$name;?>
-						</option>
-					<?php endforeach; ?>
-				</select>
-			</form>
+			<a class="brand" href="#">Torneos de Q+D</a>
+			<div class="nav-collapse">
+				<ul class="nav">
+					<li><a href="/"><?=_('home');?></a></li>
+		
+					<?php if($this->tank_auth->is_logged_in()): ?>
+						<li><a href="/tournament/"><?=_('tournaments');?></a></li>
+						<li><a href="/player/"><?=_('players');?></a></li>
+						<li><a href="/team/"><?=_('teams');?></a></li>
+						<li><a href="/auth/logout"><?=_('Logout');?></a></li>
+					<?php endif; ?>
+				</ul>
+				
+				<form action="#" class="pull-right">
+					<select name="language_chooser" class="input-small">
+						<?php foreach($languages as $key => $name): ?>
+							<option value="<?=$key?>" <?=$key==$selected_language?'selected="selected"':'';?>>
+								<?=$name;?>
+							</option>
+						<?php endforeach; ?>
+					</select>
+				</form>
+			</div>
 		</div>
 	</div>
 </div>
