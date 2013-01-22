ef1018f1a20bcaa2f09bdca14b7067db2d1ee749
Justin P <ajpalmer@gmail.com>
2013-01-22 17:08:30 +0100

    Trim trailing blanks

--- system/application/controllers/player.php
+++ system/application/controllers/player.php
@@ -5,29 +5,29 @@ class Player extends GS_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		
+
 		if(!$this->tank_auth->is_logged_in())
 		{
 			$this->session->set_userdata(array('login_return' => $this->uri->uri_string()));
 			redirect('/auth/login/');
 		}
-		
+
 		$this->data['breadcrumbs'][] = array(
 			'url' => '/player/',
 			'text' => _('Players')
 		);
 	}
-	
+
 	function index()
 	{
 		$this->data['title'] = _("Players");
 		$this->data['active_players'] = $this->player_model->getAll();
 		$this->data['old_players'] = $this->player_model->getAll(false);
-		
+
 		$this->data['content_view'] = 'players/index';
 		$this->load->view('skeleton', $this->data);
 	}
-	
+
 	function view($id)
 	{
 		$this->data['player'] = $this->player_model->get($id);
@@ -38,30 +38,30 @@ class Player extends GS_Controller {
 		} else {
 			$this->data['title'] = _("Player not found");
 		}
-		
+
 		$this->data['breadcrumbs'][] = array(
 			'url' => '/player/view/'.$id,
 			'text' => $this->data['player']->username
 		);
-		
+
 		$this->data['content_view'] = 'players/view';
 		$this->load->view('skeleton', $this->data);
 	}
-	
+
 	function create()
 	{
 		if(!$this->tank_auth->is_admin())
 			header('Location: /');
-		
+
 		$this->load->helper(array('form', 'url'));
 		$this->load->library('form_validation');
-		
+
 		$this->data['title'] = _('New player');
-		
+
 		$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean|min_length['.$this->config->item('username_min_length', 'tank_auth').']|max_length['.$this->config->item('username_max_length', 'tank_auth').']');
 		$this->form_validation->set_rules('email', 'Email', 'trim|required|xss_clean|valid_email');
 		$this->form_validation->set_rules('sex', 'Sex', 'required|xss_clean');
-		
+
 		if($this->form_validation->run() == FALSE)
 		{
 			$this->data['content_view'] = 'players/create';
@@ -72,34 +72,34 @@ class Player extends GS_Controller {
 				$this->input->post('email'),
 				$this->input->post('sex')
 			);
-			
+
 			header('Location: /player/view/'.$id);
 		}
 	}
-	
+
 	function edit($id)
 	{
 		if(!$this->tank_auth->is_admin(array('player' => $id)))
 			header('Location: /');
-		
+
 		$this->load->helper(array('form', 'url'));
 		$this->load->library('form_validation');
-		
+
 		$this->data['player'] = $this->player_model->get($id);
-		
+
 		$this->data['title'] = sprintf(_('Edit player %s'), $this->data['player']->username);
-		
+
 		$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean|min_length['.$this->config->item('username_min_length', 'tank_auth').']|max_length['.$this->config->item('username_max_length', 'tank_auth').']');
 		$this->form_validation->set_rules('email', 'Email', 'trim|required|xss_clean|valid_email');
 		$this->form_validation->set_rules('password', 'Password', 'trim|xss_clean|max_length['.$this->config->item('password_max_length', 'tank_auth').']|alpha_dash');
 		$this->form_validation->set_rules('confirm_password', 'Confirm Password', 'trim|xss_clean|matches[password]');
 		$this->form_validation->set_rules('sex', 'Sex', 'required|xss_clean');
-		
+
 		$this->data['breadcrumbs'][] = array(
 			'url' => '/player/view/'.$id,
 			'text' => $this->data['player']->username
 		);
-		
+
 		if($this->form_validation->run() == FALSE)
 		{
 			$this->data['content_view'] = 'players/edit';
@@ -112,30 +112,30 @@ class Player extends GS_Controller {
 				$this->input->post('password'),
 				$this->input->post('sex')
 			);
-			
+
 			header('Location: /player/view/'.$id);
 		}
 	}
-	
+
 	function disable($player_id)
 	{
 		if(!$this->tank_auth->is_admin())
 			header('Location: /');
-		
+
 		$this->player_model->disable($player_id);
-			
+
 		header('Location: /player/view/'.$player_id);
 	}
-	
+
 	function enable($player_id)
 	{
 		if(!$this->tank_auth->is_admin())
 			header('Location: /');
-		
+
 		$this->player_model->enable($player_id);
-			
+
 		header('Location: /player/view/'.$player_id);
 	}
 }
-	
+
 ?>
