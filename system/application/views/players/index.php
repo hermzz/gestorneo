14b25ea834cb3487b5e899a6c5477b0bec857ffd
Justin P <ajpalmer@gmail.com>
2013-01-22 17:14:28 +0100

    Tabbed Players index
    Move current and ex players onto different tabs

--- system/application/views/players/index.php
+++ system/application/views/players/index.php
@@ -1,38 +1,46 @@
-<?php if($this->tank_auth->is_admin()): ?>
-	<ul class="nav nav-tabs">
-		<li class="dropdown pull-left" data-dropdown="dropdown">
-			<a href="#" class="dropdown-toggle">Admin <b class="caret"></b></a>
-			<ul class="dropdown-menu">
-				<li><a href="/player/create/"><?=_('New player');?></a></li>
-			</ul>
-		</li>
-	</ul>
-<?php endif; ?>
-
-<h2><?=_('Players');?></h2>
-
-<?php if(count($active_players) > 0): ?>
-	<h3><?=_('Active');?></h3>
-	<ul>
-		<?php foreach($active_players as $player): ?>
-			<li><a href="<?=site_url('/player/view/'.$player->id)?>">
-				<?=$player->username?></a>
+	<div class="tabbable">
+		<ul class="nav nav-tabs">
+	    <li class="active"><a href="#tab1" data-toggle="tab"><?=_('Active');?></a></li>
+	    <li><a href="#tab2" data-toggle="tab"><?=_('The exes');?></a></li>
+	<?php if($this->tank_auth->is_admin()): ?>
+			<li class="dropdown pull-left" data-dropdown="dropdown">
+				<a href="#" class="dropdown-toggle">Admin <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<li><a href="/player/create/"><?=_('New player');?></a></li>
+				</ul>
 			</li>
-		<?php endforeach; ?>
-	</ul>
-<?php else: ?>
-	<p><?=_('No active players.');?></p>
-<?php endif; ?>
+	<?php endif; ?>
+		</ul>
+	  <div class="tab-content">
+			<h2><?=_('Players');?></h2>
+		  <div class="tab-pane active" id="tab1">
+				<?php if(count($active_players) > 0): ?>
+					<h3><?=_('Active');?></h3>
+					<ul>
+						<?php foreach($active_players as $player): ?>
+							<li><a href="<?=site_url('/player/view/'.$player->id)?>">
+								<?=$player->username?></a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				<?php else: ?>
+					<p><?=_('No active players.');?></p>
+				<?php endif; ?>
 
-<?php if(count($old_players) > 0): ?>
-	<h3><?=_('The exes');?></h3>
-	<ul>
-		<?php foreach($old_players as $player): ?>
-			<li><a href="<?=site_url('/player/view/'.$player->id)?>">
-				<?=$player->username?></a>
-			</li>
-		<?php endforeach; ?>
-	</ul>
-<?php else: ?>
-	<p><?=_('No inactive players.');?></p>
-<?php endif; ?>
+		  </div>
+		  <div class="tab-pane" id="tab2">
+				<?php if(count($old_players) > 0): ?>
+					<h3><?=_('The exes');?></h3>
+					<ul>
+						<?php foreach($old_players as $player): ?>
+							<li><a href="<?=site_url('/player/view/'.$player->id)?>">
+								<?=$player->username?></a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				<?php else: ?>
+					<p><?=_('No inactive players.');?></p>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
