450e9af6e094ae10f80e8fb645aa97aa467dd298
Justin P <ajpalmer@gmail.com>
2013-01-28 16:09:25 +0100

    Responsive navbar
    Fix the responsive navbar at width < 980px (show the icons, remove the
    upper padding and float the language selector left)

--- system/application/views/navbar.php
+++ system/application/views/navbar.php
@@ -1,7 +1,14 @@
 <div class="navbar navbar-fixed-top navbar-inverse">
 	<div class="navbar-inner">
 		<div class="container">
-			<div class="nav-collapse">
+
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+			<div class="nav-collapse collapse">
 				<ul class="nav">
 					<li><a href="/"><?=_('home');?></a></li>
 
