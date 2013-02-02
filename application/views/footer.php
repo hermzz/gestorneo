b566407576388a9d596e9a342fd968552e41f2a7
Hermann Kaser <hermann.kaser@gmail.com>
2013-02-02 23:47:34 +0000

    Font credits

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,4 @@
 <div id="footer" class="fill">
-	<p>Powered by <a href="https://github.com/hermzz/gestorneo" target="_blank">gestorneo</a></p>
+	<p>Powered by <a href="https://github.com/hermzz/gestorneo" target="_blank">gestorneo</a>. 
+		Entypo pictograms by Daniel Bruce — <a href="http://www.entypo.com">www.entypo.com</a></p>
 </div>
