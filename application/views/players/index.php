d38e013d76eb651cce56daa8f7ba5894cab1662b
sirreal <jon.surrell@gmail.com>
2015-02-01 19:19:27 +0100

    tabs & spaces

--- application/views/players/index.php
+++ application/views/players/index.php
@@ -1,7 +1,7 @@
 	<div class="tabbable">
 		<ul class="nav nav-tabs">
-	    <li class="active"><a href="#tab1" data-toggle="tab"><?=_('Active');?></a></li>
-	    <li><a href="#tab2" data-toggle="tab"><?=_('The exes');?></a></li>
+			<li class="active"><a href="#tab1" data-toggle="tab"><?=_('Active');?></a></li>
+			<li><a href="#tab2" data-toggle="tab"><?=_('The exes');?></a></li>
 	<?php if($this->tank_auth->is_admin()): ?>
 			<li class="dropdown pull-left" data-dropdown="dropdown">
 				<a href="#" class="dropdown-toggle">Admin <b class="caret"></b></a>
@@ -11,9 +11,9 @@
 			</li>
 	<?php endif; ?>
 		</ul>
-	  <div class="tab-content">
+		<div class="tab-content">
 			<h2><?=_('Players');?></h2>
-		  <div class="tab-pane active" id="tab1">
+			<div class="tab-pane active" id="tab1">
 				<?php if($active_players): ?>
 					<h3><?=_('Active');?></h3>
 					<ul>
@@ -27,8 +27,8 @@
 					<p><?=_('No active players.');?></p>
 				<?php endif; ?>
 
-		  </div>
-		  <div class="tab-pane" id="tab2">
+			</div>
+			<div class="tab-pane" id="tab2">
 				<?php if($old_players): ?>
 					<h3><?=_('The exes');?></h3>
 					<ul>
