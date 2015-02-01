a922ccd188359fbe4bc5c7b61822a660ca2a2f7d
sirreal <jon.surrell@gmail.com>
2015-02-02 00:04:11 +0100

    working practice implementation

--- application/models/practice_model.php
+++ application/models/practice_model.php
@@ -30,10 +30,7 @@ class Practice_model extends CI_Model
 			// 2015-02-01 00:00:00
 			$mysql_date = date('Y-m-d H:i:s', strtotime($row['repeat_rule'], $php_date));
 
-			$this->db
-				->update('practices as t')
-				->set('t.next_date', $mysql_date)
-				->where('t.id', $row['id']);
+			$this->db->update('practices as t', "t.next_date = '$mysql_date'", "t.id = {$row['id']}");
 		}
 
 		return $this;
