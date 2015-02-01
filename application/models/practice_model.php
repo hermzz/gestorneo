0f5ed03f42b1aa05444cc46475efb1658199444e
sirreal <jon.surrell@gmail.com>
2015-02-02 00:19:19 +0100

    working practice implementation

--- application/models/practice_model.php
+++ application/models/practice_model.php
@@ -26,15 +26,11 @@ class Practice_model extends CI_Model
 
 		foreach ($query as $row)
 		{
-			$php_date = mysql_to_unix($row['next_date']);
-			// 2015-02-01 00:00:00
-			$mysql_date = date('Y-m-d H:i:s', strtotime($row['repeat_rule'], $php_date));
-
-			var_export($php_date);
-			echo PHP_EOL;
-			var_export($mysql_date);
-
-			$this->db->update('practices as t', "t.next_date = '$mysql_date'", "t.id = {$row['id']}");
+			$this->db->update(
+				'practices AS t',
+				't.next_date = DATE_ADD(t.next_date, INTERVAL 1 WEEK)',
+				't.next_date < NOW() AND t.recurring IS TRUE'
+			);
 		}
 
 		return $this;
